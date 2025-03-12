7e3b5f536576c22f0bf5ac26653706ce2ed8da8f
unknown <Korvo@DESKTOP-88FBGE6>
2025-03-12 15:15:24 +0100

    serveur deploy

--- src/Views/Contact/Contact.jsx
+++ src/Views/Contact/Contact.jsx
@@ -20,7 +20,7 @@ function Contact() {
       messageNotCheck[0].textContent ="";
 
       await axios
-        .post("https://softwebsolutions/api", {
+        .post("https://softwebsolutions:8000/api", {
           lastName: lastName,
           firstName: firstName,
           phoneNumber: phoneNumber,
